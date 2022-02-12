9186a1f996a405e4a59b91d5f74ba0d6dc73266b
lianpf <lianpf1@guahao.com>
2022-02-12 16:27:20 +0800

    delete

--- js/grid.js
+++
@@ -1,1 +0,0 @@
-"use strict";function initGrid(){var r=$(".dream-grid").masonry({itemSelector:".dream-column"});r.imagesLoaded().progress(function(){return r.masonry("layout")})}$(document).ready(initGrid);
